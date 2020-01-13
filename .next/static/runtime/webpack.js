5eadb5d29a96c9119dec769327e6283289256957
hamadou diallo <hamadou_diallo@yahoo.com>
2020-01-13 17:21:07 -0500

    fix

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "47da07b065531fa38ec7";
+/******/ 	var hotCurrentHash = "63cc8327ecc1c36c5ec2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
