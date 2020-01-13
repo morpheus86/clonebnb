dedf280edf8f8f04f16b7fabd156595a184ed60c
hamadou diallo <hamadou_diallo@yahoo.com>
2020-01-13 17:19:20 -0500

    link added

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "47da07b065531fa38ec7";
+/******/ 	var hotCurrentHash = "166d091a539a758184c7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ .next/static/webpack/static/development/pages/houses/[id].js.f36f25b74a8b9a78af43.hot-update.js
@@ -0,0 +1,36 @@
+webpackHotUpdate("static/development/pages/houses/[id].js",{
+
+/***/ "./pages/houses/[id].js":
+/*!******************************!*\
+  !*** ./pages/houses/[id].js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/hamadoudiallo/Desktop/airbnbClone/pages/houses/[id].js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Test"));
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=[id].js.f36f25b74a8b9a78af43.hot-update.js.map
