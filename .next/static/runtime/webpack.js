e9921e59e300f4e49dbd390b42f683fd5d82e0f3
hamadou diallo <hamadou_diallo@yahoo.com>
2020-02-29 18:44:12 -0500

    some fixes

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c10a5032ecb8a6258170";
+/******/ 	var hotCurrentHash = "0b340682712bd023786d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
