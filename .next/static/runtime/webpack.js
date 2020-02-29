83e5b0db2831c2f74aaa6361b02d9f82f9778c92
hamadou diallo <hamadou_diallo@yahoo.com>
2020-02-29 16:51:03 -0500

    Login and Registration UI fixed

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "22edadb0abdeb9401e25";
+/******/ 	var hotCurrentHash = "7f8388e1d9f6a84e74b8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
