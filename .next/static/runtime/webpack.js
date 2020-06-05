31778617a643ba0d89becc53e55f7b6abbe546cd
hamadou diallo <hamadou_diallo@yahoo.com>
2020-06-05 00:17:01 -0400

    add deployment link and finishing touches

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a0247039a85aa5757b33";
+/******/ 	var hotCurrentHash = "22afb990db04241d5134";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Header.js
+++ components/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
                 {user ? (
                   <ul>
                     <li>
-                      <Link href="/bookings/bookings">
+                      <Link href="/bookings/bookings" as="/bookings/bookings">
                         <a>Bookings</a>
                       </Link>
                     </li>
