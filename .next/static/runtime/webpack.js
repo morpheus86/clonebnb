e38b94712a60cfcf85dab78a8735d1ec5172432e
hamadou diallo <hamadou_diallo@yahoo.com>
2020-06-05 00:32:33 -0400

    add deployment link and finishing touches

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "22afb990db04241d5134";
+/******/ 	var hotCurrentHash = "12c8e4e6700b26c993ab";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
