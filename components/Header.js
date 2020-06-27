6e18138d0d8e9fd3d301981e8f252fa53d5349d0
hamadou diallo <hamadou_diallo@yahoo.com>
2020-06-27 14:37:27 -0400

    resolving booking rooting error

--- components/Header.js
+++ components/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
                 {user ? (
                   <ul>
                     <li>
-                      <Link href="/bookings" as="/bookings">
+                      <Link href="/Bookings" as="/bookings">
                         <a>Bookings</a>
                       </Link>
                     </li>
